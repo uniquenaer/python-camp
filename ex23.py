78ba5945637c5f99aaa84ca0ac577464141d3d10
nana <1774732369@qq.com>
2019-04-24 10:13:37 +0800

    feat:test tag

---
+++ ex23.py
@@ -0,0 +1,13 @@
+# 测试 tag 使用方法
+
+# git tag 命令
+
+# git tag 名字 –m "注释"
+
+# git tag 1.0.3 -m "发布版本1.0.3"
+
+# git push origin tag名
+
+# 删除远程Tag：git push origin --delete Tag名
+
+# 删除本地Tag git tag -d Tag名
